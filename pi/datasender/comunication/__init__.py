0c9c716a4fdb739e2f05007744bcf1c09067f86f
Jonah Wille <jonah.wille@gea.com>
2025-01-21 15:44:17 +0100

    Add error handling and success message for data sending attempts

--- pi/datasender/comunication/__init__.py
+++ pi/datasender/comunication/__init__.py
@@ -12,6 +12,7 @@ class DataSender():
         self.url = url
 
     def sendMesurment(self, rpm, flow, trys=5):
+        error = False
         for try_ in range(trys):
             # print(
             # f"Sending data to {self.url} \trpm: {rpm}\tflow: {flow}")
@@ -24,9 +25,13 @@ class DataSender():
                 response = requests.post(self.url, json=data, timeout=1)
                 if response.status_code // 100 != 2:
                     raise Exception(f"Status code {response.status_code}")
+                if error:
+                    print(f"Request successfull after {try_+1} atempts")
                 return
             except Exception as ex:
-                print(f"Error sending data to server. Try {try_+1}/{trys}")
+                error = True
+                print(
+                    f"Error sending data to server. Try {try_+1}/{trys} {ex}")
                 sleep(1)
                 if try_ >= trys-1:
                     raise ConectionError("Error sending data to server")
